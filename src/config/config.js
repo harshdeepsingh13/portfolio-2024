304749182bf0077fec8927bb4cb02870e92cb27a
Harshdeep Singh <harshdeepsingh13@gmail.com>
2023-12-23 00:56:27 -0800

    implemented Resume Download page.

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,20 @@
-import styled from "styled-components";
-
 export const LINK = {
-	HOME: "/",
-	SKILLS: "/skills",
-	PROJECTS: "/projects",
-	EXPERIENCES: "/experiences",
-	EDUCATION: "/education",
-	RESUME: "/resume",
-	SEARCH: "/search"
+    HOME: "/",
+    SKILLS: "/skills",
+    PROJECTS: "/projects",
+    EXPERIENCES: "/experiences",
+    EDUCATION: "/education",
+    RESUME: "/resume",
+    SEARCH: "/search"
 }
 
 export const API_ROUTES = {
-	BASE_URL: "/api/user",
-	GET_BASIC_INFORMATION: "/basicInformation",
-	GET_SKILLS: "/skillInformation",
-	GET_PROJECTS: "/projectInformation",
-	GET_EXPERIENCES: "/workExperienceInformation",
-	GET_EDUCATION_DETAILS: "/educationInformation"
+    BASE_URL: "/api/user",
+    GET_BASIC_INFORMATION: "/basicInformation",
+    GET_SKILLS: "/skillInformation",
+    GET_PROJECTS: "/projectInformation",
+    GET_EXPERIENCES: "/workExperienceInformation",
+    GET_EDUCATION_DETAILS: "/educationInformation"
 }
+
+export const cloudinaryResLink = process.env.REACT_APP_CLOUDINARY_RES_LINK;
--- src/containerComponents/ResumePage/index.js
+++ src/containerComponents/ResumePage/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Container} from "../../GlobalStyles";
+import {Container, PageHeader} from "../../GlobalStyles";
+import {faDownload} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {DownloadButton} from "./styles";
+import {cloudinaryResLink} from "../../config/config";
 
 const ResumePage = props => {
-return <>
-    <Container>
-	    Resume Page
-    </Container>
+    const onResumeDownload = () => {
+        let a = document.createElement('a');
+        a.target = '_blank';
+        a.href = `${cloudinaryResLink}Resume%20-%20Harshdeep%20Singh.pdf`;
+        a.click();
+    };
 
-</>
+    return <>
+        <Container>
+            <PageHeader>Download Resume</PageHeader>
+
+            <DownloadButton onClick={onResumeDownload}>
+                <FontAwesomeIcon icon={faDownload}/>
+                Download Resume
+            </DownloadButton>
+        </Container>
+
+    </>
 };
 
-ResumePage.propTypes={
+ResumePage.propTypes = {
     props: PropTypes.object
 };
-ResumePage.defaultProps={
+ResumePage.defaultProps = {
     props: {}
 };
 
