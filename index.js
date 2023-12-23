ec7f4c13f1536d4cebbb358802767a6568869749
Harshdeep Singh <harshdeepsingh13@gmail.com>
2023-12-23 02:41:11 -0800

    added forwarding to react build on server.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const mongooseConnection = require('./config/mongoose.js');
 const apiRoutes = require("./api");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8081;
@@ -18,5 +19,11 @@ app.use("/api", apiRoutes)
 
 //mongodb connection
 mongooseConnection();
+
+//React forwarding
+app.use(express.static('./build'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'))
+})
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
