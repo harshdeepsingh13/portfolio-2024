4c5534d0f8df2c511929fbe67df36eb5e8a1ff34
Harshdeep Singh <harshdeepsingh13@gmail.com>
2023-12-23 09:19:33 -0800

    metadata updates & Google Analytics bug fixes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from "react-router-dom";
 
 import ReactGA from "react-ga4";
 
-ReactGA.initialize(process.env.REACT_APP_GA4_MEASUREMENT_ID);
+ReactGA.initialize(process.env.REACT_APP_GA4_MEASUREMENT_ID, {gtagOptions: {"config": process.env.REACT_APP_GA4_MEASUREMENT_ID}});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
