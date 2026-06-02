5734193fae8caa6bc451930407590716522010d3
Harshdeep Singh <harshdeepsingh@Harshdeeps-MacBook-Pro.local>
2026-06-02 00:49:32 -0700

    feat: make sitemap dynamic so new blog posts appear without rebuild

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { getData } from "@/lib/getData";
 
+export const dynamic = "force-dynamic";
+
 const siteUrl = "https://theharshdeepsingh.com";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
